878aa75751fae7f1b7e89f7ee7db0b7c29d4322c
Modey Codey <phawngam@hotmail.com>
2024-05-15 13:39:28 +0700

    Ex1

---
+++ script.js
@@ -0,0 +1,18 @@
+// Ex1.1
+// Ans 7 
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+console.log(myAlphabet.length)
+
+// Ex1.2
+let planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"];
+planets.forEach(function(planets, index) {
+    console.log(index, planets);
+});
+
+// ex1.3
+let myArr = [ 1, 2, 'One', true];
+console.log(myArr);
+
+myArr.forEach(function(myArr) {
+    console.log(myArr);
+});
